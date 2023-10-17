b31c32e7210e0846844c0704e3f71c85b2b8f61e
abdelrhmaneladawy26 <abdelrhmaneladawy26@gmail.com>
2023-10-17 07:14:30 +0300

    create routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/home", element: <HomePage /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
